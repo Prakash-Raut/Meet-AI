f0c883d6d759d2d18f8e9305f868f07178297d08
Prakash Raut <prakash_raut@outlook.com>
2025-05-30 13:06:16 +0530

    feat: integrate TRPC and React Query for data fetching, update layout to include TRPC provider

--- components/home/home.tsx
+++ components/home/home.tsx
@@ -1,31 +1,15 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { authClient } from "@/lib/auth-client";
-import { useRouter } from "next/navigation";
+import { useTRPC } from "@/trpc/client";
+import { useQuery } from "@tanstack/react-query";
 
 export default function Home() {
-	const router = useRouter();
-	const { data: session } = authClient.useSession();
-
-	if (!session) {
-		return <p>Loading...</p>;
-	}
+	const trpc = useTRPC();
+	const { data } = useQuery(trpc.hello.queryOptions({ text: "Prakash" }));
 
 	return (
-		<main className="flex flex-col items-center justify-center h-screen p-4 gap-4">
-			<p>Logged in as {session.user.name}</p>
-			<Button
-				onClick={() =>
-					authClient.signOut({
-						fetchOptions: {
-							onSuccess: () => router.push("/sign-in"),
-						},
-					})
-				}
-			>
-				Sign out
-			</Button>
+		<main className="flex flex-col p-4 gap-y-4">
+			<p>{data?.greeting}</p>
 		</main>
 	);
 }
